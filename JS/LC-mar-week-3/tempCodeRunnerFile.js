261865325df1763f54e40ede9f52b64dc17393e4
HappiRisu <lisu5336@gmail.com>
2024-03-12 22:23:28 +0900

    334 done

---
+++ JS/LC-mar-week-3/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+
+//   let arr_asc = nums.sort((a, b) => b - a);
+//   console.log(arr_asc[2] < arr_asc[1], arr_asc[1] < arr_asc[0])
+//   return arr_asc[2] < arr_asc[1] && arr_asc[1] < arr_asc[0]
+// };
